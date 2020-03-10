db8b181ceb706703b2601e8d5b8b2cbc00f0e96a
Shawn Taylor <shawncurtistaylor@hotmail.com>
2020-03-10 16:11:24 -0700

    initial commit

---
+++ ResistorColor.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+
+public static class ResistorColor
+{
+    public static int ColorCode(string color)
+    {
+        var colors = new Dictionary<string, int>()
+        {
+            { "black", 0},
+            { "brown", 1},
+            { "red", 2 },
+            { "orange", 3 },
+            { "yellow", 4 },
+            { "green", 5 },
+            { "blue", 6 },
+            { "violet", 7 },
+            { "grey", 8 },
+            { "white", 9 },
+        };
+
+        return colors[color];
+    }
+
+    public static string[] Colors()
+    {
+        return new[] { "black", "brown", "red", "orange", "yellow", "green", "blue", "violet", "grey", "white" };
+    }
+}
